07936098767c03dc67af35b97012ab7aa4b26f63
EasyLife <yazeed.k22016@gmail.com>
2024-10-08 17:41:15 +0200

    take this

--- src/main/java/com/example/demo6/HelloApplication.java
+++ src/main/java/com/example/demo6/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
@@ -19,6 +19,5 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-//yazeed yazeded besersefsefs
     }
 }
