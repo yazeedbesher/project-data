e4852aca0bf8a153bfeaa9188efd17b02c003e0f
EasyLife <yazeed.k22016@gmail.com>
2024-10-08 17:42:09 +0200

    take this

--- src/main/java/com/example/demo6/HelloApplication.java
+++ src/main/java/com/example/demo6/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
-        stage.setTitle("");
+        stage.setTitle("hi");
         stage.setScene(scene);
         stage.show();
     }
