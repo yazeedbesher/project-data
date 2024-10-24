d9c6614153bf106ad4e4cd1fb5ea344f1316cc86
AhmadMayyaleh122 <mayahmad336@gmail.com>
2024-10-24 20:10:19 +0300

    ahmad 24/10 abo alakh

--- src/main/java/com/example/demo6/MainCont.java
+++ src/main/java/com/example/demo6/MainCont.java
@@ -1,8 +1,14 @@
 package com.example.demo6;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class MainCont {
 
@@ -23,8 +29,24 @@ public class MainCont {
     @FXML
     private MenuButton VButton;
 
+    @FXML
+    private AnchorPane mainpage;
+
+    @FXML
+    private AnchorPane Homepage;
 
 
+    @FXML
+    public void Vehiclepage() throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("Vehicle.fxml"));
+        mainpage.getChildren().setAll(root);
+    }
 
+    @FXML
+    public void Mainpage() throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Homepage.getChildren().setAll(root);
+
+    }
 
 }
