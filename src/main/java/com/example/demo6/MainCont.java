69cdb075a3a0dd089be43eac27a0a63d16990ced
AhmadMayyaleh122 <mayahmad336@gmail.com>
2024-10-24 15:53:15 +0300

    main and the main controller

---
+++ src/main/java/com/example/demo6/MainCont.java
@@ -0,0 +1,27 @@
+package com.example.demo6;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+
+public class MainCont {
+
+    @FXML
+    private MenuButton ACC_INV_Button;
+    @FXML
+    private Button HomeButton;
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    private MenuButton SUP_Button;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    private MenuButton VButton;
+
+
+}
