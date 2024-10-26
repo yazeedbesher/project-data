ca2038356bebc8ca10631b02e940bd3887d8ea9b
AhmadMayyaleh122 <mayahmad336@gmail.com>
2024-10-26 12:43:21 +0200

    ahmad 26/10 12:45pm

--- src/main/java/com/example/demo6/HelloApplication.java
+++ src/main/java/com/example/demo6/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 760);
         
         stage.setScene(scene);
         stage.show();
