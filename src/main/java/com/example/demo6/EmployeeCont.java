80d4b8c85cbbb7b2c3cdff963434391b8c41c3f1
EasyLife <yazeed.k22016@gmail.com>
2024-11-02 19:57:03 +0200

    wdadawdaw

--- src/main/java/com/example/demo6/EmployeeCont.java
+++ src/main/java/com/example/demo6/EmployeeCont.java
@@ -6,7 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.sql.*;
 
 public class EmployeeCont {
 
@@ -55,8 +57,46 @@ public void Update_Complaints_click(){
   }
 
     @FXML
+    private TextArea comparea;
+    @FXML
     public void Update_list_click(){
-        /////بنطهر الشكاوي من الداتا
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "1221";
+
+            // Establishing connection
+            Connection conn = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM communication";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            this.comparea.setText("Id\tRec Type\t\t\tTitle\t\t\t\tStatus\t\t\t\tDate\t\t\tResponse\n");
+            this.comparea.appendText("---------------------------------------------------------------------------------------------------------\n");
+
+
+            comparea.setStyle("-fx-control-inner-background: #65929e;");
+            comparea.setEditable(false);
+            while (rs.next()) {
+                int deptNo = rs.getInt("recordid");
+                String rec = rs.getString("recordtype");
+                String tit = rs.getString("title");
+                Date date = rs.getDate("date_and_time");
+                String stat = rs.getString("status");
+                String res = rs.getString("response_requird");
+
+                this.comparea.appendText(deptNo + "\t"+ rec + tit+ "\t\t"+stat + "\t"+date+ "\t\t"
+                        +res+  "\n");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+
+
 
     }
 
