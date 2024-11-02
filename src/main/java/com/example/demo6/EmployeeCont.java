872dd998c5243fda597c0d1e7fcf76174d264bda
EasyLife <yazeed.k22016@gmail.com>
2024-11-03 00:42:14 +0200

    wdadawdaw

--- src/main/java/com/example/demo6/EmployeeCont.java
+++ src/main/java/com/example/demo6/EmployeeCont.java
@@ -157,10 +157,225 @@ public void Update_Complaints_click(){
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e.toString());
         }
-
-
-
     }
+    @FXML
+    private TextArea hreq;
+
+    @FXML
+    private TextArea preq;
+
+    @FXML
+    private TextArea vreq;
+    public void vreq(){
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "1221";
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM vrequest";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            this.vreq.setText("ReqId\tSSN\t\t\t\tPhone\t\t\tEmployeeName\n");
+            this.vreq.appendText("---------------------------------------------------------------------------\n");
+
+            vreq.setStyle("-fx-control-inner-background: #65929e;");
+            vreq.setEditable(false);
+            while (rs.next()) {
+                int reqid = rs.getInt("requestid");
+                String ssn = rs.getString("ssn");
+                int ph = rs.getInt("phone");
+                String emp = rs.getString("employeename");
+
+                this.vreq.appendText(reqid+ "\t\t"+ssn + "\t"+ "\t\t"+ph+ "\t\t"
+                        +emp+  "\n");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
+    public void preq(){
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "1221";
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM prequest";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            this.preq.setText("ReqId\tSSN\t\t\t\tPhone\n");
+            this.preq.appendText("---------------------------------------------------------------------------\n");
+
+            preq.setStyle("-fx-control-inner-background: #65929e;");
+            preq.setEditable(false);
+            while (rs.next()) {
+                int reqid = rs.getInt("requestid");
+                String ssn = rs.getString("ssn");
+                int ph = rs.getInt("phone");
 
 
+                this.preq.appendText(reqid+ "\t\t"+ssn + "\t"+ "\t\t"+ph+ "\t\t"+
+                        "\n");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
+    public void hreq(){
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "1221";
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM hrequest";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            this.hreq.setText("ReqId\tSSN\t\t\t\tPhone\t\t\tHospitalName\n");
+            this.hreq.appendText("---------------------------------------------------------------------------\n");
+
+            hreq.setStyle("-fx-control-inner-background: #65929e;");
+            hreq.setEditable(false);
+            while (rs.next()) {
+                int reqid = rs.getInt("requestid");
+                String ssn = rs.getString("ssn");
+                int ph = rs.getInt("phone");
+                String hp = rs.getString("hospitalname");
+
+
+                this.hreq.appendText(reqid+ "\t\t"+ssn + "\t"+ "\t\t"+ph+ "\t\t"+ "\t\t"+hp+
+                        "\n");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
+    @FXML
+    private TextArea hinsur;
+
+    @FXML
+    private TextArea pinsur;
+
+    @FXML
+    private TextArea vinsur;
+    public void vinsur(){
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "1221";
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM vehicle";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            this.vinsur.setText("Id\tName\t\t\tPrice\t\tVehcile\t\t\tType\n");
+            this.vinsur.appendText("---------------------------------------------------------------------------\n");
+
+            vinsur.setStyle("-fx-control-inner-background: #65929e;");
+            vinsur.setEditable(false);
+            while (rs.next()) {
+                int reqid = rs.getInt("policyid");
+                String name = rs.getString("policyholdername");
+                int pc = rs.getInt("price");
+                String vt = rs.getString("vehicletype");
+                String ins = rs.getString("insurancetype");
+
+
+                this.vinsur.appendText(reqid+ "\t"+name + "\t"+pc+ "\t\t"+vt+ "\t\t\t"+ins+
+                        "\n");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
+    public void pinsur(){
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "1221";
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM proparty";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            this.pinsur.setText("Id\tName\t\t\t\tArea\t\tType\t\t\t\tPrice\n");
+            this.pinsur.appendText("---------------------------------------------------------------------------\n");
+
+            pinsur.setStyle("-fx-control-inner-background: #65929e;");
+            pinsur.setEditable(false);
+            while (rs.next()) {
+                int reqid = rs.getInt("policyid");
+                String name = rs.getString("policyholdername");
+                int area = rs.getInt("coverageamount");
+                String vt = rs.getString("coverage_Type");
+                int pc = rs.getInt("price");
+
+
+                this.pinsur.appendText(reqid+ "\t"+name +  "\t\t"+area+ "\t"+vt+"\t\t\t"+pc+"\n");
+
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
+    public void hinsur() {
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "1221";
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM health";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            this.hinsur.setText("Id\tName\t\t\tType\t\t\t\tProvider\t\t\t\tPrice\n");
+            this.hinsur.appendText("-----------------------------------------------------------------------------------------\n");
+
+            hinsur.setStyle("-fx-control-inner-background: #65929e;");
+            hinsur.setEditable(false);
+            while (rs.next()) {
+                int reqid = rs.getInt("policyid");
+                String name = rs.getString("policyholdername");
+                String type = rs.getString("propartytype");
+                String pr = rs.getString("provider");
+                int pc = rs.getInt("price");
+
+
+                this.hinsur.appendText(reqid + "\t" + name + "\t" + type + "\t\t\t" + pr + "\t\t\t" + pc + "\n");
+
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
 }
